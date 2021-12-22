26a338dce70b928cb8b2c6dbd30d76d62a9094dc
Daniil <danya.zinovenko@mail.ru>
2021-12-22 22:06:27 +0300

    de

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -18,7 +18,7 @@ class ProfileStatus extends React.Component {
         this.setState({
             editMode: false
         });
-        this.props.updateStatus(this.state.s tatus);
+        this.props.updateStatus(this.state.status);
     }
 
     onStatusChange = (e) => {
